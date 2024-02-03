29dcb317eebc9b9f0baaf0fc31d51e51bf78d215
Pi1at <Pi1at@users.noreply.github.com>
2024-02-03 17:11:30 +0300

    Initial commit
    add README.md
    add LICENSE
    add tasks description
    direnv & flake for developing on NixOS

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        rustToolchain =
+          pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        nativeBuildInputs = with pkgs; [ dioxus-cli rustToolchain pkg-config ];
+        buildInputs = with pkgs; [ ];
+      in
+      {
+        devShells.default =
+          pkgs.mkShell { inherit buildInputs nativeBuildInputs; };
+      });
+}
