b2ea4519250e90cc0ffd76c484e093cd330df839
Pi1at <Pi1at@users.noreply.github.com>
2024-02-06 23:07:42 +0300

    test: add quick and dirty simple tests to socket module

--- smarthome/src/devices/socket.rs
+++ smarthome/src/devices/socket.rs
@@ -38,3 +38,59 @@ impl SmartSocket {
         }
     }
 }
+// region:    --- Tests
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_description_matches_name() {
+        let name = "TestSocket";
+        let ts = SmartSocket::new(name.into());
+        assert_eq!(ts.description(), name);
+    }
+
+    #[test]
+    fn test_switching_on_off() {
+        let mut ss1 = SmartSocket {
+            name: "Socket1".into(),
+            state: SmartSocketState::On,
+        };
+        // let mut ss2 = SmartSocket {
+        //     name: "Socket2".into(),
+        //     state: SmartSocketState::Off,
+        // };
+        let s3 = ss1.clone();
+        let mut ts = SmartSocket::new("TestSocket".into());
+
+        println!("Working with socket {ts:?}");
+
+        // after creation we have State::Off
+        assert_eq!(ts.state, SmartSocketState::Off);
+
+        ts.turn_on();
+        // we can turnng on
+        assert_eq!(ts.state, SmartSocketState::On);
+
+        ts.turn_on();
+        // even twice in a row
+        assert_eq!(ts.state, SmartSocketState::On);
+
+        // get some power when turned on
+        assert_ne!(ts.get_current_power_usage(), 0);
+
+        ts.turn_off();
+        // we can turn it off
+        assert_eq!(ts.state, SmartSocketState::Off);
+
+        ts.turn_off();
+        // even twice in a row
+        assert_eq!(ts.state, SmartSocketState::Off);
+
+        // No power when turned off
+        assert_eq!(ts.get_current_power_usage(), 0);
+    }
+}
+
+// endregion: --- Tests
