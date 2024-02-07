529f259ecc4660558db7d80ad9fd5b845e11df87
Pi1at <Pi1at@users.noreply.github.com>
2024-02-07 14:43:22 +0300

    style(smarthome): cleanup

--- smarthome/src/lib.rs
+++ smarthome/src/lib.rs
@@ -4,13 +4,3 @@
 pub mod devices;
 pub mod location;
 pub mod providers;
-
-#[cfg(test)]
-mod tests {
-    //use super::*;
-
-    // #[test]
-    // fn it_works() {
-    //     //TODO: реализовать в дальнейшем тесты
-    // }
-}
