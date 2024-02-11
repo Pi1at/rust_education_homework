45497f4fad6ddbf4affa1f96cea07d776f888bfe
Pi1at <Pi1at@users.noreply.github.com>
2024-02-11 17:21:10 +0300

    refactor(location): create DeviceLocation trait

--- smarthome/src/location/room.rs
+++ smarthome/src/location/room.rs
@@ -3,23 +3,41 @@ use std::{borrow::Borrow, collections::HashSet};
 
 // Помещение содержит названия нескольких устройств.
 // Устройство имеет уникальное в рамках помещения имя.
+// TODO: Better naming
+pub trait DeviceLocation {
+    #[must_use]
+    fn new(name: RoomName) -> Self;
+    fn add_device(&mut self, device: DeviceName);
+}
+
 #[derive(Clone, Eq, PartialEq)]
 pub struct Room {
     pub(crate) name: RoomName,
     pub(crate) devices: HashSet<DeviceName>,
 }
 
-impl Room {
+impl DeviceLocation for Room {
     #[must_use]
-    pub fn new(name: RoomName) -> Self {
+    fn new(name: RoomName) -> Self {
         Self {
             name,
             devices: HashSet::new(),
         }
     }
-    pub fn add_device(&mut self, device: DeviceName) {
+    fn add_device(&mut self, device: DeviceName) {
         self.devices.insert(device);
     }
+}
+impl Room {
+    #[must_use]
+    pub fn new(name: RoomName) -> Self {
+        DeviceLocation::new(name)
+    }
+
+    pub fn add_device(&mut self, device: DeviceName) {
+        DeviceLocation::add_device(self, device);
+    }
+
     #[must_use]
     pub fn with_device(mut self, device: DeviceName) -> Self {
         self.devices.insert(device);
