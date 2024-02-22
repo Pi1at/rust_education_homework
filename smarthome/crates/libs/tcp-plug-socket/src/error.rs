8e136db8df26f6cba3976c6e36d69e4cfaadde71
Pi1at <Pi1at@users.noreply.github.com>
2024-02-22 09:27:36 +0300

    docs(tcp-plug-socket): add comments for error variants

--- smarthome/crates/libs/tcp-plug-socket/src/error.rs
+++ smarthome/crates/libs/tcp-plug-socket/src/error.rs
@@ -6,8 +6,10 @@ use crate::{Command, OddResponse};
 
 #[derive(Debug, Error)]
 pub enum Error {
+    /// Error indicating a wrong response was received for a given command.
     #[error("wrong response {resp} for command {cmd}")]
     WrongResponse { cmd: Command, resp: OddResponse },
+    /// Wrapper around the standard IO error to integrate with custom error handling.
     #[error("IO error")]
     IOError(#[from] io::Error),
 }
